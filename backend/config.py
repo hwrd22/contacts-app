0b882355ac5bd6988896bace1f5c5b093bec2f88
hwrd22 <113211442+hwrd22@users.noreply.github.com>
2024-05-27 14:18:49 +0800

    Replaced the static and joke-y JWT secret key with a key randomly generated by secrets.token_hex()

--- backend/config.py
+++ backend/config.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+import secrets
 
 app = Flask(__name__)
 CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contacts-user.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["JWT_SECRET_KEY"] = "contacts-app-lol"
+app.config["JWT_SECRET_KEY"] = secrets.token_hex(32)
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
